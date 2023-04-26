ab426d1e2d63104734334196c3bb16afcbe2cf2a
Adri Rommers <a3.p.rommers@gmail.com>
2023-04-26 13:56:22 +0200

    passing arguments

--- passing_args.c
+++ passing_args.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   threads.c                                          :+:    :+:            */
+/*   passing_args.c                                     :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: arommers <arommers@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2023/04/26 13:26:51 by arommers      #+#    #+#                 */
-/*   Updated: 2023/04/26 13:27:30 by arommers      ########   odam.nl         */
+/*   Created: 2023/04/26 13:28:30 by arommers      #+#    #+#                 */
+/*   Updated: 2023/04/26 13:53:47 by arommers      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,45 @@
 #include <unistd.h>
 #include <pthread.h>
 
-void	*routine(void)
+int	primes[10] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
+
+void	*routine(void *arg)
 {
-	printf("test\n");
-	sleep (3);
-	printf("ending thread\n");
+	int index;
+
+	index = *(int*)arg;
+	printf("%d ", primes[index]);
+	free(arg);
 	return (NULL);
 }
 
 int	main(int argc, char **argv)
 {
-	pthread_t	t1;
-	pthread_t	t2;
+	int			i;
+	int			j;
+	int			*a;
+	pthread_t	thread[10];
 
-	if (pthread_create(&t1, NULL, &routine, NULL) != 0)
-		return (1);
-	if (pthread_create(&t2, NULL, &routine, NULL) != 0)
-		return (2);
-	if (pthread_join(t1, NULL) != 0)
-		return (3);
+	i = 0;
+	j = 0;
+	a = malloc(sizeof(int));
+	if (!a)
+		perror("malloc");
+	while (i < 10)
+	{
+		a = malloc(sizeof(int));
+		if (!a)
+			perror("malloc");
+		*a = i;
+		if (pthread_create(&thread[i], NULL, &routine, a) != 0)
+			perror("ERROR 1");
+		i++;
+	}
+	while (j < 10)
+	{
+		if (pthread_join(thread[j], NULL) != 0)
+			perror("ERROR 2");
+		j++;
+	}
 	return (0);
 }
