c4cb354a1a0c8c7bc264a15969939c462f171e94
Adri Rommers <a3.p.rommers@gmail.com>
2023-05-03 15:08:39 +0200

    added main

---
+++ src/main.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.c                                             :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: arommers <arommers@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/05/03 15:01:37 by arommers      #+#    #+#                 */
+/*   Updated: 2023/05/03 15:08:19 by arommers      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	main(int argc, char **argv)
+{
+	t_data	*data;
+	t_philo	*philo;
+
+	if (input_check(argc, argv != 0))
+		return (1);
+	if (initialize(argc, argv, data, philo) != 0)
+		return (1);
+	simulate(data, philo);
+	return (0);
+}
