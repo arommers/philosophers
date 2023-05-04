1f6774143e30035c5afd1efcc623156982d52864
Adri Rommers <a3.p.rommers@gmail.com>
2023-05-04 12:37:53 +0200

    further init

--- include/philo.h
+++ include/philo.h
@@ -6,7 +6,7 @@
 /*   By: arommers <arommers@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/05/03 10:21:54 by arommers      #+#    #+#                 */
-/*   Updated: 2023/05/03 15:40:51 by arommers      ########   odam.nl         */
+/*   Updated: 2023/05/04 12:23:08 by arommers      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,18 +20,24 @@
 # include <sys/time.h>     // for gettimeofday
 # include <pthread.h>      // for pthread & mutex
 
-typedef struct s_data{
-	int					philo_nr;
+typedef struct s_data {
+	int					nr_philos;
 	int					meal_nbr;
 	int					done;
 	int					status;
+	int					who_died;
 	unsigned long		time_to_die;
 	unsigned long		time_to_eat;
 	unsigned long		time_to_sleep;
 	pthread_mutex_t		*forks;
 }	t_data;
 
-typedef struct s_philo{
+typedef struct s_fork {
+	pthread_mutex_t mutex;
+	bool_t			taken;
+}
+
+typedef struct s_philo {
 	int					id;
 	int					meals_eaten;
 	unsigned long		last_meal;
@@ -41,6 +47,5 @@ typedef struct s_philo{
 	t_data				*data;
 
 }	t_philo;
-
 
 #endif
--- src/init.c
+++ src/init.c
@@ -6,15 +6,28 @@
 /*   By: arommers <arommers@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/05/03 15:09:07 by arommers      #+#    #+#                 */
-/*   Updated: 2023/05/03 16:03:07 by arommers      ########   odam.nl         */
+/*   Updated: 2023/05/04 11:18:22 by arommers      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+int	init_forks(t_data *data)
+{
+	int	i;
+
+	i = 0;
+	data->forks = malloc(data->nr_philos * sizeof(pthread_mutex_t));
+	if (!data->forks)
+		return (1);
+	while (i < data->nr_philos)
+		pthread_mutex_init(&data->forks[i++], NULL);
+	return (0);
+}
+
 int	init_data(int argc, char **argv, t_data *data)
 {
-	data->philo_nr = (int)atolong(argv[1]);
+	data->nr_philos = (int)atolong(argv[1]);
 	data->time_to_die = atolong(argv[2]);
 	data->time_to_eat = atolong(argv[3]);
 	data->time_to_sleep = atolong(argv[4]);
@@ -24,6 +37,7 @@ int	init_data(int argc, char **argv, t_data *data)
 		data->meal_nbr = -1;
 	data->done = 0;
 	data->status = 0;
+	data->who_died = 0;
 	if (init_forks(data) != 0)
 		return (1);
 	return (0);
@@ -33,11 +47,16 @@ int	init_philos(t_data *data, t_philo *philos)
 {
 	int	i;
 
-	i = 1;
-	while (i < data->philo_nr)
+	i = 0;
+	while (i < data->nr_philos)
 	{
-		philos->id = i;
-		philos->data = data;
+		philos[i].id = i + 1;
+		philos[i].meals_eaten = 0;
+		philos[i].last_meal = get_time();
+		philos[i].l_fork = &data->forks[i];
+		philos[i].r_fork = &data->forks[(i + 1) % data->nr_philos];
+		philos[i].print = NULL;
+		philos[i].data = data;
 	}
 }
 
@@ -50,7 +69,7 @@ int	initialize(int argc, char **argv, t_data *data, t_philo	*philos)
 		return (1);
 	}
 	init_data(argc, argv, data);
-	philos = (t_philo *)malloc(sizeof(t_philo) * data->philo_nr);
+	philos = (t_philo *)malloc(sizeof(t_philo) * data->nr_philos);
 	if (!philos)
 	{
 		// free data struct;
